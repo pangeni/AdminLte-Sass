7b611ad1ba26d2db6ccc13878124d7d8fce80316
pangeni <pangenisanjay@gmail.com>
2022-08-14 12:51:05 +0545

    config resolve path in app.js and app.scss

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 window._ = _;
 
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+
+import 'bootstrap'
+
+import 'admin-lte'
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
