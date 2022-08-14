fc22d19c7ff1c8facd48ff1677045c19686a4b89
pangeni <pangenisanjay@gmail.com>
2022-08-14 12:47:52 +0545

    resolve path for boostrap admin lte and fontawesome

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,4 +9,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    refresh: [
+        'routes/**',
+        'resources/views/**',
+    ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~adminLte': path.resolve(__dirname, 'node_modules/admin-lte'),
+            '~fontawesome': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free'),
+        }
+    }
 });
